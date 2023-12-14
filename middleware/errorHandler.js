1b382cc34d68ba09ef692132971408ed18ba9200
Zeon Zhang <976006787@qq.com>
2023-12-15 03:29:08 +1100

    JWT auth

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,3 +1,4 @@
+
 const { logEvents } = require('./logger')
 
 const errorHandler = (err, req, res, next) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)
+
 
 router.route('/')
     .get(usersController.getAllUsers)
